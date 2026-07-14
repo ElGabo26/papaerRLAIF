e689f1a9e4cd5579d9bbecb2074c9bf230e1b935
ElGabo26 <jcgab2009@gmail.com>
2026-07-14 13:08:31 -0500

    tools

--- codPreferences/testFunctions.py
+++ codPreferences/testFunctions.py
@@ -2,7 +2,7 @@ import pandas as pd
 from tools import *
 
 #lista de  modelos
-modelsName=["deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B 1.5B",
+modelsName=["deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B",
 "Qwen/Qwen2.5-1.5B-Instruct 1.5B",
 "meta-llama/Llama-3.2-1B-Instruct"]
 LOCALROOT="/"
