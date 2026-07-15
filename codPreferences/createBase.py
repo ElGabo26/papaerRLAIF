8660e5e5dd8d3559b41f60c8232cd016f1a2e088
ElGabo26 <jcgab2009@gmail.com>
2026-07-15 11:55:33 -0500

    make all bases

--- codPreferences/createBase.py
+++ codPreferences/createBase.py
@@ -4,17 +4,15 @@ from time import time
 from  tools import  makeResponse, testModel
 from metrics import medir_recursos
 
-RUTA="/workspace/models/Llama-3.2-1B-Instruct"
 REPETITIONS=4
 RUTAOUTPUT="/workspace/papaerRLAIF/codPreferences/bases"
 base=pd.read_csv("codPreferences/prompts.csv")
 prompts=base.sample(frac=1.0, random_state=42)
 prompts1=prompts['prompt'].values
-
 total=len(prompts1)
 
-token, model= testModel(RUTA)
-result=prompts.copy()
+
+
 
 def getresults(token, model,x):
     t0=time()
@@ -25,16 +23,20 @@ def getresults(token, model,x):
     return x,r,t1-t0, despues['ram_mb'], despues['gpu_mb']
 
 
-for i in range(REPETITIONS): 
-    print(f"repeticion_{i+1}")
-    columnas=['prompt',f'response_{i+1}',f'tiempo_{i+1}',f'ram_mb_{i+1}',f'gpu_mb_{i+1}']  
-    with tqdm(total=total) as barra:
-        resultado = list(map(
-            lambda x: getresults(token, model,x),
-            prompts1))
-    r=pd.DataFrame(columns=columnas,data=resultado)
-    name='result'+RUTA.split('/')[-1]
-    r.to_csv(f"{RUTAOUTPUT}/{name}_{i+1}.csv")
-    print(f"RESPUESTAS: {i+1} REALIZADAS")
+for ruta in ['/workspace/models/DeepSeek-R1-Distill-Qwen-1.5B',
+             '/workspace/models/Qwen2.5-1.5B-Instruct']:
+    print("MODELO: ", ruta)
+    token, model= testModel(ruta)    
+    for i in range(REPETITIONS): 
+        print(f"repeticion_{i+1}")
+        columnas=['prompt',f'response_{i+1}',f'tiempo_{i+1}',f'ram_mb_{i+1}',f'gpu_mb_{i+1}']  
+        with tqdm(total=total) as barra:
+            resultado = list(map(
+                lambda x: getresults(token, model,x),
+                prompts1))
+        r=pd.DataFrame(columns=columnas,data=resultado)
+        name='result'+ruta.split('/')[-1]
+        r.to_csv(f"{RUTAOUTPUT}/{name}_{i+1}.csv")
+        print(f"RESPUESTAS: {i+1} REALIZADAS")
 
 
