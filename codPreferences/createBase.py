551c68f40530be96c2e3659389ecd316a9cb82b9
ElGabo26 <jcgab2009@gmail.com>
2026-07-15 09:20:25 -0500

    creacion  base

--- codPreferences/createBase.py
+++ codPreferences/createBase.py
@@ -1,27 +1,46 @@
 import pandas as pd
+from time import time
 from  tools import  makeResponse, testModel
+from metrics import medir_recursos
 
-RUTA='/workspace/models/Qwen2.5-1.5B-Instruct'
+RUTA=input("inserte ruta  del  modelo: ")
+REPETITIONS=int("Inserte Cantidad de  respuestas: ")
+RUTAOUTPUT="/workspace/papaerRLAIF/codPreferences/bases"
 base=pd.read_csv("codPreferences/prompts.csv")
-prompts=base.sample(30, random_state=42)
+prompts=base.sample(frac=0.1, random_state=42)
 prompts1=prompts['prompt'].values
 
 token, model= testModel(RUTA)
-
-response=[]
-c=0
-for p in prompts1:
-    r=makeResponse(token,model,p,0.25)
-    response.append(r)
-    print(len(prompts)-c)
-    c+=1
-    
-
 result=prompts.copy()
 
-result['response']=response
+for i in range(REPETITIONS): 
+    response=[]
+    timeR=[]
+    ram=[]
+    gpu=[]
+    c=0
+    for p in prompts1:
+        t0=time()
+        antes =medir_recursos()
+        r=makeResponse(token,model,p,0.25)
+        despues=medir_recursos()
+        t1=time()
+        tr=t1-t0
+        timeR.append(tr)
+        response.append(r)
+        ram.append(despues['ram_mb']-antes['ram_mb'])
+        gpu.append(despues['gpu_mb']-antes['gpu_mb'])
+        print(len(prompts)-c)
+        c+=1
+
+    result[f'response_{i+1}']=response
+    result[f'time_{i+1}']=timeR
+    result[f'ram_mb_{i+1}']=ram
+    result[f'gpu_mb_{i+1}']=gpu
+    print(f"RESPUESTAS: {i+1} REALIZADAS")
+
 
 name='result'+RUTA.split('/')[-1]
-result.to_csv(f"{name}.csv")
+result.to_csv(f"{RUTAOUTPUT}/{name}.csv")
 
 
