e15f9b194fd6192a01928dba357fd708b128e090
ElGabo26 <jcgab2009@gmail.com>
2026-07-23 08:40:05 -0500

    m

--- codTraining/cabezales/createCabezales.py
+++ codTraining/cabezales/createCabezales.py
@@ -16,7 +16,7 @@ from transformers import AutoModel, AutoTokenizer
 # ============================================================
 
 MODEL_DOWLOAD= "/microsoft/deberta-v3-large"
-MODEL_ROUTE="./workspace/models/deberta-v3-large"
+MODEL_ROUTE="/workspace/models/deberta-v3-large"
 DATABASE_ROUTE="codTraining/cabezales/trainingCabezales/claridad.csv"
 OUTPUT_ROUTE="codTraining/cabezales/vectorBases"
 SEED =42
