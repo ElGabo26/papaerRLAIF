edbbc925e0cbfcf3c2b049284e7bdf0c45ab8ce7
ElGabo26 <jcgab2009@gmail.com>
2026-07-27 10:47:26 -0500

    m

--- codTraining/cabezales/makeCabezales.py
+++ codTraining/cabezales/makeCabezales.py
@@ -119,8 +119,12 @@ for i,j in params1.items():
         print('MODELO ENTRENADO')
         #evaluamos el modelo
         criterio = nn.CrossEntropyLoss()
-        valores_unicos = torch.unique(test.tensors[1])
-        print("Elementos únicos TEST:", valores_unicos)
+        
+        etiquetas = torch.tensor([label for _, label in test])
+
+        valores_unicos = torch.unique(etiquetas)
+        print("Valores únicos:", valores_unicos)
+        
         resultadoTest=eval_multiclass(model,testL,DEVICE,criterio,class_names)
         resultadoVal=eval_multiclass(model,evalL,DEVICE,criterio,class_names)
         print('MODELO EVALUADO')
