cd62b48a91afb5da49289b527911a60800a2d75f
ElGabo26 <jcgab2009@gmail.com>
2026-07-29 15:45:47 -0500

    deep

--- ppo_rlhf_complete/train_ppo.py
+++ ppo_rlhf_complete/train_ppo.py
@@ -31,10 +31,10 @@ from trl.experimental.ppo import PPOConfig, PPOTrainer
 # Los parámetros están agrupados por función. Los parámetros marcados como
 # PRIORITARIOS son los primeros que conviene variar durante la experimentación.
 
-PREFERENCES_CSV="/workspace/papaerRLAIF/codTraining/calify/preferencesDeep.csv"
-MODEL_PATH="/workspace/models/DeepSeek-R1-Distill-Qwen-1.5B"
-OUTPUT_MODEL_PATH="/workspace/adaptedModels/PPO/finalPPOModels/DeepSeek-R1-Distill-Qwen-1.5B-PPO"
-REWARD_MODEL_PATH="/workspace/adaptedModels/PPO/mergedRewardModels/DeepSeek-R1-Distill-Qwen-1.5B"
+PREFERENCES_CSV="/workspace/papaerRLAIF/codTraining/calify/preferencesLlama.csv"
+MODEL_PATH="/workspace/models/Llama-3.2-1B-Instruct"
+OUTPUT_MODEL_PATH="/workspace/adaptedModels/PPO/finalPPOModels/Llama-3.2-1B-Instruct-PPO"
+REWARD_MODEL_PATH="/workspace/adaptedModels/PPO/mergedRewardModels/Llama-3.2-1B-Instruct"
 
 from typing import Any
 
